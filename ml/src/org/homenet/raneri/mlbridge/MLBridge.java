3b194c417d6eadb9b827b4b99ce5db80462f15b9
Kevin Raneri <kevin.raneri@gmail.com>
2019-03-10 11:40:48 -0400

    Added optimization to reduce differing material penalties

--- ml/src/org/homenet/raneri/mlbridge/MLBridge.java
+++ ml/src/org/homenet/raneri/mlbridge/MLBridge.java
@@ -113,6 +113,54 @@ public class MLBridge {
 
             Thread.sleep(10);
         }
+
+        System.out.println("Beginning like-sized material optimization.");
+        lastSuccessful = iterations;
+
+        while (true) {
+            iterations++;
+
+            int[] changedMembers = new int[changingMembersPerIteration];
+
+            for (int i = 0; i < changingMembersPerIteration; i++) {
+                int changingMember = random.nextInt(bridge.getMembers().size());
+
+                Shape oldShape = bridge.getMembers().get(changingMember).getShape();
+                boolean canIncreaseShapeSize = (bridge.getInventory().getAllowedShapeChanges(oldShape) & Inventory.SHAPE_INCREASE_SIZE) == Inventory.SHAPE_INCREASE_SIZE;
+                if (!canIncreaseShapeSize) {
+                    i--;
+                } else {
+                    changedMembers[i] = changingMember;
+                    bridge.getMembers().get(changingMember).setShape(bridge.getInventory().getShape(oldShape, 1));
+                }
+            }
+
+            result = Analysis.analyze(bridge);
+
+            if (!result.getPassed() || result.getCost() > previousCost) {
+                // Revert
+                for (int i = 0; i < changingMembersPerIteration; i++) {
+                    bridge.getMembers().get(changedMembers[i]).setShape(bridge.getInventory().getShape(bridge.getMembers().get(changedMembers[i]).getShape(), -1));
+                }
+            } else {
+                System.out.println(result.getCost());
+                lastSuccessful = iterations;
+                previousCost = result.getCost();
+                try {
+                    bridge.write(new File("bridgeout/bridge" + iterations + ".bdc"));
+                } catch (IOException e) {
+                    System.out.println("Failed to write bridge!");
+                }
+            }
+
+            if (lastSuccessful + 1000 < iterations) {
+                // Probably no more improvements to be made.
+                System.out.println("Finished optimization. Reduced cost by " + (double) Math.round((1 - bridge.getTotalCost() / initialCost) * 1000) / 10 + "%");
+                break;
+            }
+
+            Thread.sleep(10);
+        }
     }
 
 }
